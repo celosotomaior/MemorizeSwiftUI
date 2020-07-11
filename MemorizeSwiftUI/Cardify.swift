581a59518485e4a96f4fabc48e2067cfd97ddb75
Marcelo Sotomaior <marcelo.sotomaior@gmail.com>
2020-07-11 03:34:53 +0100

    - Animation implemented

--- MemorizeSwiftUI/Cardify.swift
+++ MemorizeSwiftUI/Cardify.swift
@@ -25,14 +25,14 @@ struct Cardify: AnimatableModifier {
     
     func body(content: Content) -> some View {
         ZStack {
-            if isFaceUp {
+            Group {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                 content
-            } else {
-                
-                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
+            .opacity(isFaceUp ? 1 : 0)
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
+                    .opacity(isFaceUp ? 0 : 1)
             
         }
         .rotation3DEffect(Angle.degrees(rotation), axis: (x: 0, y: 1, z: 0))
--- MemorizeSwiftUI/EmojiMemoryGameView.swift
+++ MemorizeSwiftUI/EmojiMemoryGameView.swift
@@ -14,7 +14,7 @@ struct EmojiMemoryGameView: View {
         VStack {
             Grid(viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
-                    withAnimation(.linear(duration:2)) {
+                    withAnimation(.linear(duration:0.75)) {
                     self.viewModel.choose(card: card)
                     }
                 }
@@ -41,12 +41,34 @@ struct CardView: View {
         
     }
     
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     @ViewBuilder
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched {
             ZStack {
-                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                Group {
+                if card.isConsumingBonusTime {
+                    Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-animatedBonusRemaining*360-90), clockwise: true)
+                        
+                        .onAppear() {
+                            self.startBonusTimeAnimation()
+                    }
+                } else {
+                    Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-card.bonusRemaining*360-90), clockwise: true)
+                        
+                       
+                    }
+                }
                     .padding(5).opacity(0.4)
+                .transition(.identity)
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
                     .rotationEffect(Angle.degrees((card.isMatched ? 360:0)))
